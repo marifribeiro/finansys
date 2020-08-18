7bf92ac559566bcca353b82626ecebfe30a581c3
Mariana Fernandes Ribeiro <mariana.ribeiro@pravaler.com.br>
2020-08-18 19:18:36 -0300

    Adição do serviço de DB in memory

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,38 @@
+import { InMemoryDbService, RequestInfo } from 'angular-in-memory-web-api';
+
+export class InMemoryDatabase implements InMemoryDbService {
+  createDb() {
+
+    const categories = [
+      {
+        id: 1,
+        name: "Lazer",
+        description: "Cinema, parque, viagens, etc."
+      },
+      {
+        id: 2,
+        name: "Moradia",
+        description: "Pagamentos de contas da casa"
+      },
+      {
+        id: 3,
+        name: "Saúde",
+        description: "Plano de saúde e remédios"
+      },
+      {
+        id: 4,
+        name: "Salário",
+        description: "Recebimento do salário"
+      },
+      {
+        id: 5,
+        name: "Freelas",
+        description: "Trabalhos como freelancer."
+      },
+      
+    ]
+
+    return { categories };
+  }
+  
+}
