3c70cc48c3f530bf668b3e1e3d97574de9c3e3e2
Mariana Fernandes Ribeiro <mariana.ribeiro@pravaler.com.br>
2020-08-25 19:36:50 -0300

    Adicionando service na listagem de categorias e implementando método deleteCategory

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Category } from '../shared/category.model';
+import { CategoryService} from '../shared/category.service';
+
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
+
+  constructor(private categoryService: CategoryService ) { }
 
   ngOnInit(): void {
+    this.categoryService.getAll().subscribe(
+      categories => this.categories = categories,
+      error => console.log(error)
+    )
+  }
+
+  deleteCategory(category) {
+    const mustDelete = confirm(`Deseja excluir a categoria ${category.name}?`);
+
+    if (mustDelete) {
+      this.categoryService.delete(category.id).subscribe(
+        () => {
+          this.categories = this.categories.filter(el => el !== category)
+        },
+        error => {
+          console.log(`Erro ao excluir => ${error}`);
+        }
+      )
+    }
   }
 
 }
