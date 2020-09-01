4a68c8efed1ab8000a6196b9d075ef35d19cda01
Mariana Fernandes Ribeiro <mariana.ribeiro@pravaler.com.br>
2020-09-01 19:32:47 -0300

    Implementação dos lançamentos

---
+++ src/app/pages/entries/entry-list/entry-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Entry } from '../shared/entry.model';
+import { EntryService} from '../shared/entry.service';
+
+@Component({
+  selector: 'app-entry-list',
+  templateUrl: './entry-list.component.html',
+  styleUrls: ['./entry-list.component.css']
+})
+export class EntryListComponent implements OnInit {
+
+  entries: Entry[] = [];
+
+  constructor(private entryService: EntryService ) { }
+
+  ngOnInit(): void {
+    this.entryService.getAll().subscribe(
+      entries => this.entries = entries,
+      error => console.log(error)
+    )
+  }
+
+  deleteEntry(entry) {
+    const mustDelete = confirm(`Deseja excluir a categoria ${entry.name}?`);
+
+    if (mustDelete) {
+      this.entryService.delete(entry.id).subscribe(
+        () => {
+          this.entries = this.entries.filter(el => el !== entry)
+        },
+        error => {
+          console.log(`Erro ao excluir => ${error}`);
+        }
+      )
+    }
+  }
+
+}
