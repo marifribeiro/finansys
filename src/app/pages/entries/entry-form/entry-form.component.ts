4c6311710f3de6a60327b63a5d26218611cf56a9
Mariana Fernandes Ribeiro <mariana.ribeiro@pravaler.com.br>
2020-09-08 19:53:49 -0300

    Adicionando campos do formulário de lançamentos

---
+++ src/app/pages/entries/entry-form/entry-form.component.ts
@@ -0,0 +1,163 @@
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Entry } from '../shared/entry.model';
+import { EntryService } from '../shared/entry.service';
+
+import toastr from 'toastr';
+import { switchMap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-entry-form',
+  templateUrl: './entry-form.component.html',
+  styleUrls: ['./entry-form.component.css']
+})
+
+export class EntryFormComponent implements OnInit, AfterContentChecked {
+
+  currentAction: string;
+  entryForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessages: string[] = null;
+  submittingForm: boolean = false;
+  entry: Entry = new Entry;
+
+  imaskConfig = {
+    mask: Number,
+    scale: 2,
+    thousandsSeparator: '.',
+    padFractionalZeros: true,
+    normalizeZeros: true,
+    radix: ','
+  }
+
+  ptBR = {
+    firstDayOfWeek: 0,
+    dayNames: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
+    dayNamesShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sat"],
+    dayNamesMin: ["D", "S", "T", "Q", "Q", "S", "S"],
+    monthNames: [
+      "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+      "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+    monthNamesShort: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+    today: 'Hoje',
+    clear: 'Limpar',
+    dateFormat: 'dd/mm/yy'
+  };
+
+  constructor(
+    private entryService: EntryService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.setCurrentAction();
+    this.buildEntryForm();
+    this.loadEntry();
+  }
+
+  ngAfterContentChecked() {
+    this.setPageTitle();
+  }
+
+  submitForm() {
+    this.submittingForm = true;
+
+    if (this.currentAction === "new") {
+      this.createEntry();
+    } else {
+      this.updateEntry();
+    }
+  }
+
+  private setCurrentAction() {
+    if (this.route.snapshot.url[0].path === "new") {
+      this.currentAction = 'new';
+    } else {
+      this.currentAction = 'edit'
+    }
+  }
+
+  private buildEntryForm() {
+    this.entryForm = this.formBuilder.group({
+      id: [null],
+      name: [null, [Validators.required, Validators.minLength(2)]],
+      description: [null],
+      type: [null, [Validators.required]],
+      amount: [null, [Validators.required]],
+      date: [null, [Validators.required]],
+      paid: [null, [Validators.required]],
+      categoryId: [null, [Validators.required]]
+    })
+  }
+
+  private loadEntry() {
+    if (this.currentAction === 'edit') {
+      this.route.paramMap.pipe(
+        switchMap(params => this.entryService.getById(+params.get("id")))
+      )
+      .subscribe(entry => {
+          this.entry = entry;
+          this.entryForm.patchValue(entry)
+      },
+      error => alert('Erro no servidor'))
+    }
+  }
+
+  private setPageTitle() {
+    if (this.currentAction === 'new') {
+      this.pageTitle = 'Cadastro de novo lançamento';
+    } else {
+      const entryName = this.entry.name || "";
+      this.pageTitle = `Editando lançamento: ${entryName}`;
+    }
+  }
+
+  createEntry() {
+    const entry: Entry = Object.assign(new Entry(), this.entryForm.value);
+
+    this.entryService.create(entry)
+    .subscribe(
+      entry => this.actionsForSuccess(entry),
+      error => this.actionsForError(error)
+    )
+  }
+
+  private updateEntry() {
+    const entry: Entry = Object.assign(new Entry(), this.entryForm.value);
+
+    this.entryService.update(entry)
+    .subscribe(
+      entry => this.actionsForSuccess(entry),
+      error => this.actionsForError(error)
+    )
+  }
+
+  actionsForSuccess(entry: Entry) {
+    if (this.currentAction === "new") {
+      toastr.success("Novo lançamento adicionada com sucesso!");
+    } else {
+      toastr.success("Lançamento editado com sucesso!")
+    }
+
+    this.router.navigateByUrl('entries', {skipLocationChange: true})
+    .then(
+      () => this.router.navigate(["entries", entry.id, "edit"])
+    )
+  }
+
+  actionsForError(error) {
+    toastr.error("Ops! Algo deu errado :(");
+
+    this.submittingForm = false;
+
+    if (error.status === 422) {
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+    } else {
+      this.serverErrorMessages = ["Falha na comunicação com o servidor. Tente novamente mais tarde."]
+    }
+  }
+}
